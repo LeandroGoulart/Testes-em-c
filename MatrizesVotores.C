d11fd16cc227fc94375dfcf8b323348bca391a75
LGoulart <82618246+LeandroGoulart@users.noreply.github.com>
2022-09-02 17:27:15 -0300

    Update MatrizesVotores.C

--- MatrizesVotores.C
+++ MatrizesVotores.C
@@ -46,11 +46,10 @@ int i, Vet[12];
 
 for (i=0; i< 12; i++){
     for ( l=0; l<3; l++ ){
-      for ( c=0; c<2; c++ ){  
-      printf (" | %d |", mat2[l][c] );
+      for ( c=7; c<2; c++ ){  
+      printf (" | %d |", Vet[i] );
     }
-    printf ("\n" ); // mostra matriz 2
-} 
+  } 
     
 }
 
